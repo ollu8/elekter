815c7fedeba01b237ccdc9c4a7c0fbc5fcd6f60b
olivern <olivern@greeny.cs.tlu.ee>
2015-03-24 11:16:44 +0200

    amprid

--- Proov2.java
+++ Proov2.java
@@ -1,7 +1,7 @@
 public class Proov2{
 	public static void main(String[] arg){
 		TakistusKomponent t=new Takisti220oomi();
-		Vooluallikas telefonilaadija=new Laadija(5, 1);
+		Vooluallikas telefonilaadija=new Laadija(5, 3.5);
 		telefonilaadija.yhendaTarbija(t);
 		System.out.println(telefonilaadija.kysiVool());
 	}
